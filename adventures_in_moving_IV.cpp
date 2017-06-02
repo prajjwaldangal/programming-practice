08de2a63ddab612fbe33332e9ae50e744ec62a0e
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-06-01 21:33:53 -0500

    added problem info

---
+++ adventures_in_moving_IV.cpp
@@ -0,0 +1,21 @@
+/*
+	1 km = 1 l
+	capacity = 200 l
+
+*/
+
+// Problem: adventures_in_moving_IV 
+// websit: kattis
+
+#include <iostream>
+#include <iterator>
+#include <sstream>
+
+int main() {
+	int dis;
+	std::cin >> dis;
+	for (std::string line; std::getline(std::cin, line);) {
+		
+	}
+	return 0;
+}
