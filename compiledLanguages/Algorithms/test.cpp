43975ca2c48b4df4e6a6dd41a2ff7affac71c82c
Prajjwal Dangal <pdangal@uccs.edu>
2022-05-04 16:09:45 -0600

    playing with a feature of c++11

--- compiledLanguages/Algorithms/test.cpp
+++ compiledLanguages/Algorithms/test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "QuickFind.cpp"
+// #include "QuickFind.cpp"
 #include "subprocess.hpp"
 
 #define MAX_NUM_NODES 16
@@ -11,12 +11,13 @@ int main() {
     int N;
     cin >> N;
     string tuples;
-    vector < tuple <int, int > > edge_list; 
-    cout << "Enter the edge list separated by a semi-colon ";
-    getline(cin, tuples);
-    printf("inp: %s\n", tuples.c_str());
-    vector <int> arr = {1,2,3,4,5,6,7,8,9};
-    //1-2-5-6-7 3-8-9-4
-    QuickFind <int> obj (arr);
+    size_t size {};
+    // vector < Edge > edge_list; 
+    // cout << "Enter the edge list separated by a semi-colon ";
+   
+    
+    // printf("inp: %s\n", tuples.c_str());
+    // //1-2-5-6-7 3-8-9-4
+    // QuickFind <int> obj (edge_list);
     return 0;
 }
