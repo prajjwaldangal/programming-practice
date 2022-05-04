708051753dbdcda24115c2e0f5f2b2cfa16560a5
Prajjwal Dangal <pdangal@uccs.edu>
2022-05-04 10:44:38 -0600

    committing before subprocess.hpp

--- compiledLanguages/Algorithms/test.cpp
+++ compiledLanguages/Algorithms/test.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "QuickFind.cpp"
+#include "subprocess.hpp"
+
+#define MAX_NUM_NODES 16
+#define MAX_INP_LEN 16 * 3 + 15
 
 using namespace std;
-
-
 int main() {
     cout << "Enter the number of nodes" << endl;
     int N;
     cin >> N;
-
-    cout << "Enter the edge list" << endl;
-
+    string tuples;
+    vector < tuple <int, int > > edge_list; 
+    cout << "Enter the edge list separated by a semi-colon ";
+    getline(cin, tuples);
+    printf("inp: %s\n", tuples.c_str());
     vector <int> arr = {1,2,3,4,5,6,7,8,9};
     //1-2-5-6-7 3-8-9-4
     QuickFind <int> obj (arr);
